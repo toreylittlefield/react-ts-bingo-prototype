cdc2c35352ff51838c8e86e0eff2ece761da8830
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-02-08 14:25:04 +0800

    user can drag with mobile. needs styling and drop

--- src/BingoBoard.tsx
+++ src/BingoBoard.tsx
@@ -1,11 +1,13 @@
 import { nanoid, random } from 'nanoid';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, Fragment } from 'react';
 import './styles.css';
 
 const DRAGGING_CLASS = 'dragging';
 
 const BG_COLOR = 'backgroundColor';
 const OPACITY = 'opacity';
+const POINTER_EVENTS = 'pointerEvents';
+const TOUCH_ACTIONS = 'touchAction';
 
 type RowOne = {
   rowOne: number;
@@ -33,6 +35,24 @@ const removeStyleFromDropRef = (dropRef: HTMLElement) => {
   const style = dropRef.style;
   style[OPACITY] = '';
   style[BG_COLOR] = '';
+};
+const addPointerStyles = (dropRef: HTMLElement) => {
+  const style = dropRef.style;
+  style[POINTER_EVENTS] = 'none';
+  style[TOUCH_ACTIONS] = 'none';
+};
+const defaultPointerStyles = (dropRef: HTMLElement) => {
+  const style = dropRef.style;
+  style[POINTER_EVENTS] = '';
+  style[TOUCH_ACTIONS] = '';
+};
+
+const addStylesToClone = (clone: HTMLElement, height: number, width: number, top?: number, left?: number) => {
+  clone.style.top = top ? `${top}px` : `0px`;
+  clone.style.left = left ? `${left}px` : `0px`;
+  clone.style.position = 'absolute';
+  clone.style.height = `${height}px`;
+  clone.style.width = `${width}px`;
 };
 
 const getDragDropTarget = (target: HTMLElement): HTMLElement | null => target.closest('[draggable]');
@@ -91,6 +111,7 @@ function checkWin(gameBoard: TileType[][]) {
 
 export default function BingoBoard() {
   const [matrix, setMatrix] = useState(matrix2D());
+  const [isEdit, setIsEdit] = useState(true);
   const dragRef = useRef<HTMLElement | null>(null);
   const dropTargetRef = useRef<HTMLElement | null>(null);
   const [gameWin, setGameWin] = useState(false);
@@ -155,41 +176,61 @@ export default function BingoBoard() {
       updateMatrixWithSwap({ rowOne, colOne }, { rowTwo, colTwo });
   };
 
-  // const handlePointerDown = (event: React.PointerEvent) => {
-  //   return;
-  //   const target = event.target as HTMLElement;
-  //   const dragTarget = target.closest('[draggable]') as HTMLElement;
-  //   if (!dragTarget) return;
-  //   const releasePointer = (event: React.PointerEvent) => () => dragTarget.releasePointerCapture(event.pointerId);
-  //   const release = releasePointer(event);
-  //   dragTarget.onpointermove = (event) => {
-  //     dragTarget.setPointerCapture(event.pointerId);
+  const releasePointer = (event: React.PointerEvent) => (dragTarget: HTMLElement) =>
+    dragTarget.releasePointerCapture(event.pointerId);
 
-  //     if (event.target instanceof HTMLElement) {
-  //       console.log(event.target.id, 'pointer move');
-  //     }
-  //   };
+  const removePointerListeners = (dragTarget: HTMLElement, release: (dragTarget: HTMLElement) => void) => {
+    dragTarget.onpointerup = null;
+    dragTarget.onpointercancel = null;
+    dragTarget.onpointermove = null;
+    defaultPointerStyles(dragTarget);
+    release(dragTarget);
+  };
 
-  //   dragTarget.onpointercancel = (event) => {
-  //     if (event.target instanceof HTMLElement) {
-  //       console.log(event.target.id, 'pointer cancel');
-  //     }
-  //     dragTarget.onpointerup = null;
-  //     dragTarget.onpointercancel = null;
-  //     dragTarget.onpointermove = null;
-  //     release();
-  //   };
+  const handlePointerDown = (event: React.PointerEvent) => {
+    // if (event.pointerType === 'mouse') return;
+    if (!isEdit) return;
+    event.preventDefault();
+    const target = event.target as HTMLElement;
+    const dragTarget = target.closest('[draggable]') as HTMLElement;
+    console.log({ dragTarget });
+    if (!dragTarget) return;
 
-  //   dragTarget.onpointerup = (event) => {
-  //     if (event.target instanceof HTMLElement) {
-  //       console.log(event.target.id, 'pointer up');
-  //     }
-  //     dragTarget.onpointerup = null;
-  //     dragTarget.onpointercancel = null;
-  //     dragTarget.onpointermove = null;
-  //     release();
-  //   };
-  // };
+    addPointerStyles(dragTarget);
+
+    dragTarget.setPointerCapture(event.pointerId);
+
+    const release = releasePointer(event);
+
+    const clone = dragTarget.cloneNode(true) as HTMLElement;
+    console.log({ clone, dragTarget });
+
+    addStylesToClone(clone, dragTarget.clientHeight, dragTarget.clientWidth);
+
+    addStyleToDropRef(dragTarget);
+    clone.style.transform = `translate(${event.clientX}px, ${event.clientY}px)`;
+    document.body.append(clone);
+
+    dragTarget.onpointermove = (event) => {
+      console.log('pointer move');
+      const { clientX, clientY } = event;
+      clone.style.transform = `translate(${clientX}px, ${clientY}px)`;
+    };
+
+    dragTarget.onpointercancel = (event) => {
+      console.log('pointer cancel');
+      removePointerListeners(dragTarget, release);
+      removeStyleFromDropRef(dragTarget);
+      clone.remove();
+    };
+
+    dragTarget.onpointerup = (event) => {
+      console.log('pointer up');
+      removePointerListeners(dragTarget, release);
+      removeStyleFromDropRef(dragTarget);
+      clone.remove();
+    };
+  };
 
   const handleClick = (event: React.MouseEvent, bool: boolean, row: number, col: number) => {
     event.preventDefault();
@@ -210,34 +251,40 @@ export default function BingoBoard() {
   }, [matrix, gameWin]);
 
   return (
-    <section className="bingo-board">
-      {matrix.map((rowArr, rowIndex) =>
-        rowArr.map(({ text, id, srcImg, isChecked }, colIndex) => (
-          <div
-            key={id}
-            id={`${rowIndex},${colIndex}`}
-            className={isChecked ? `bingo-piece checked` : `bingo-piece`}
-            onDragStart={handleDragStart}
-            draggable={true}
-            onDragEnter={handleDragEnter}
-            onDragLeave={handleDragLeave}
-            onDragOver={(e) => e.preventDefault()}
-            onDrop={handleDrop}
-            onDragEnd={handleDragEnd}
-            onClick={(event) => handleClick(event, !isChecked, rowIndex, colIndex)}
-            // onPointerDown={handlePointerDown}
-          >
-            {rowIndex === 2 && colIndex === 2 ? (
-              <p>
-                free bingo piece <sub>{text}</sub>
-              </p>
-            ) : (
-              text
-            )}
-          </div>
-        ))
-      )}
-      {gameWin && <h2>BINGO!!!!</h2>}
-    </section>
+    <Fragment>
+      <div>
+        Edit Board
+        <button onClick={() => setIsEdit((prev) => !prev)}>{isEdit ? 'On' : 'Off'}</button>
+      </div>
+      <section className="bingo-board">
+        {matrix.map((rowArr, rowIndex) =>
+          rowArr.map(({ text, id, srcImg, isChecked }, colIndex) => (
+            <div
+              key={id}
+              id={`${rowIndex},${colIndex}`}
+              className={isChecked ? `bingo-piece checked` : `bingo-piece`}
+              draggable={isEdit}
+              // onDragStart={handleDragStart}
+              // onDragEnter={handleDragEnter}
+              // onDragLeave={handleDragLeave}
+              // onDragOver={(e) => e.preventDefault()}
+              // onDrop={handleDrop}
+              // onDragEnd={handleDragEnd}
+              onClick={(event) => handleClick(event, !isChecked, rowIndex, colIndex)}
+              onPointerDown={handlePointerDown}
+            >
+              {rowIndex === 2 && colIndex === 2 ? (
+                <p>
+                  free bingo piece <sub>{text}</sub>
+                </p>
+              ) : (
+                text
+              )}
+            </div>
+          ))
+        )}
+        {gameWin && <h2>BINGO!!!!</h2>}
+      </section>
+    </Fragment>
   );
 }
